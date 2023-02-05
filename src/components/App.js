e0c4b17615d47b6e439f60be52513f06e8615793
DeepmalaDixit <117112464+DeepmalaDixit@users.noreply.github.com>
2023-02-05 21:20:19 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React,{useState,useEffect,useRef} from 'react'
+import '../styles/App.css';
+const App = () => {
+
+//code here 
+
+  return (
+    <div id="main">
+      Email
+      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
+      Password
+      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
+      <button id="submitButton" onClick={}>Submit</button><br/>
+      <p id="emailText">Your Email : {}</p>
+      <p id ="passwordText">Your Password : {}</p>
+      
+    </div>
+  )
+}
+
+
+export default App;
